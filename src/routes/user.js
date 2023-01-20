dd528cf78f997b3dcec98e9ad85592beabe1e428
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-01-21 00:16:00 +0530

    Add REST methods

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getAllUsers } = require("../controllers/user.controller");
+const { getAllUsers, getSingeUser, addUser, updateUser, deleteUser } = require("../controllers/user.controller");
 
 router.get("/", getAllUsers);
+router.get("/:id", getSingeUser);
+router.post("/", addUser);
+router.post("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
 
