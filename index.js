9a17743719b55aee2a56f56fbb9b9a2f837ad0c7
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-01-20 11:45:21 +0530

    Add test route

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const app = express();
 const usersRoutes = require("./src/routes/user");
 app.use("/users", usersRoutes);
 
+app.get("/test", (req, res) => {
+    res.status(200).json(`Server OK... ${process.pid}`);
+});
+
 app.listen(config.port, async () => {
     console.log(`:::: Server Started => PORT ${config.port} | ${config.environment} mode ::::`);
+
+    const url = `http://localhost:${config.port}/test`;
+    console.log("\nCheck status at ", url);
 });
