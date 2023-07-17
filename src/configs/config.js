9e5673f78feb9becad3ec478c1a2d29f6dcd8ea6
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-07-18 03:25:37 +0530

    Test PORT

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.SERVER1_PORT || 3000,
   environment: process.env.NODE_ENV || "development",
 };
 
