3a095661bf8f30c782d9f024e0b2521ff8de030b
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-07-18 03:27:16 +0530

    Add PORT for server2

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const config = {
-  port: process.env.SERVER1_PORT || 3000,
+  port: process.env.SERVER2_PORT || 3000,
   environment: process.env.NODE_ENV || "development",
 };
 
